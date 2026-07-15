2d6e01019da82a2148c085e82527971161852381
Miguel Angelo <migueltiojhoe@gmail.com>
2026-07-14 21:48:30 -0300

    fix: resolver badge do browser forçando start_url e purpose any

--- vite.config.js
+++ vite.config.js
@@ -17,23 +17,19 @@ export default defineConfig({
         theme_color: "#efebe1",
         background_color: "#efebe1",
         display: "standalone",
+        start_url: "/",
         icons: [
-          {
-            src: "pwa-icon.svg",
-            sizes: "any",
-            type: "image/svg+xml",
-          },
           {
             src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
           },
           {
             src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png",
-            purpose: "any maskable",
+            purpose: "any",
           }
         ],
       },
